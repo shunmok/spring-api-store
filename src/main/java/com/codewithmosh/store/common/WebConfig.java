09f1f07278f9d6cd99973a251fa11ba154c5eb7d
shunmok <shunmok7@gmail.com>
2025-05-06 13:50:25 +0800

    Fix CORS

--- src/main/java/com/codewithmosh/store/common/WebConfig.java
+++ src/main/java/com/codewithmosh/store/common/WebConfig.java
@@ -5,14 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(true)
+                .allowedOriginPatterns("*") // Add the client URL here
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .maxAge(3600);
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
